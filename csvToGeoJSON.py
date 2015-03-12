4cad82ce5fde783066f43eb3138a7745ec43cae8
Varun Ved <varunmved@gmail.com>
2015-03-11 23:45:58 -0700

    Added additional parsing features
    Now checks for quotations within a name, and if they exists it strips
    them.
    
    Also made code more modular

--- csvToGeoJSON.py
+++ csvToGeoJSON.py
@@ -1,8 +1,6 @@
 import csv
 import json
 
-
-
 featureCollectionString = """{
   "type": "FeatureCollection",
   "features": ["""
@@ -25,13 +23,24 @@ closeBracket2 = '  }'
 closeLastBracket = '}'
 closeBrace = ']'
 
-#########################
+#string writer
+def writeString():
+
+	outString = typeString + "[" + str(longi) + "," + str(lat) + "]" + '\n'+ closeBracket1+ '\n'+propertyString 
+	outString += '\n' + nameString + '"' + nameVal + '",' + "\n"+ addString + '"' + addVal + '",' 
+	outString += "\n"+ phoneString + '"' + phoneVal + '",' + "\n"+ emailString + '"' + emailVal + '",' 
+	outString += "\n"+ webString + '"' + webVal + '"' + "\n"
+	outString +=closeBracket2+'\n'+closeBracket1+'\n'
+	written.write(outString)
+
+
 
 #csv handling
 written = open("mbtestjson3.json","w")
 outString = featureCollectionString + '\n' 
 written.write(outString)
 i=0
+j=0
 
 with open("mb_for_geojson_RAW10.csv",'rU') as csvfile:
 		reader = csv.DictReader(csvfile)
@@ -43,24 +52,23 @@ with open("mb_for_geojson_RAW10.csv",'rU') as csvfile:
 			addVal=row['ADDRESS']
 			lat=row['latitude']
 			longi=row['longitude']
+			
 			if (lat=='' or longi ==''):
 				lat = '32'
 				longi='-115'
-				outString = typeString + "[" + str(longi) + "," + str(lat) + "]" + '\n'+ closeBracket1+ '\n'+propertyString 
-				outString += '\n' + nameString + '"' + nameVal + '",' + "\n"+ addString + '"' + addVal + '",' 
-				outString += "\n"+ phoneString + '"' + phoneVal + '",' + "\n"+ emailString + '"' + emailVal + '",' 
-				outString += "\n"+ webString + '"' + webVal + '"' + "\n"
-				outString +=closeBracket2+'\n'+closeBracket1+'\n'
-				written.write(outString)
+				writeString();
+			
+			if (nameVal.find('"') != -1):
+				i=i+1
+				nameVal=nameVal.strip('"')
+				j=j+1
+				writeString();
+			
 			else:
-				outString = typeString + "[" + str(longi) + "," + str(lat) + "]" + '\n'+ closeBracket1+ '\n'+propertyString 
-				outString += '\n' + nameString + '"' + nameVal + '",' + "\n"+ addString + '"' + addVal + '",' 
-				outString += "\n"+ phoneString + '"' + phoneVal + '",' + "\n"+ emailString + '"' + emailVal + '",' 
-				outString += "\n"+ webString + '"' + webVal + '"' + "\n"
-				outString +=closeBracket2+'\n'+closeBracket1+'\n'
-				written.write(outString)
-
+				writeString();
+				
 #outofloop
 outString2=closeBrace+'\n'+closeBracket2
+print(i+j)
 written.write(outString2)
 written.close()
